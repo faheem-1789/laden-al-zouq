71c13ce61d599fdbeeb3ea76317b8a2651f907d8
faheem-1789 <faheemiqbal993@gmail.com>
2025-12-03 17:13:36 +0500

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,8 +14,12 @@ export default function Navbar() {
     <nav className="bg-white shadow p-4 flex justify-between">
       <div className="font-bold text-xl">Lazzat Al Zouq</div>
       <div className="space-x-4">
-        <Link href="/profile">profile</Link>
+        <Link href="/profile">Profile</Link>
         <Link href="/menu">Menu</Link>
+        <Link href="/orders">Orders</Link>
+        <Link href="/cart">Cart</Link>  
+        <Link href="/checkout">Checkout</Link>
+
         {user ? (
           <button onClick={() => auth.signOut()}>Logout</button>
         ) : (
